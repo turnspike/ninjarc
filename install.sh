94a7bb6eea07839e82064c279ed85dfe9089a0cd
turnspike <hello@turnspike.com>
2017-09-25 13:44:25 +0930

    ni why this isn't working on RHEL, reverting logic

--- install.sh
+++ install.sh
@@ -32,11 +32,13 @@ while read file; do
   if [ ! -e $HOME/$file ]; then # file doesn't exist
     echo "linking ~/$file"
     ln -s $DIR/$file $HOME
-  elif [ -e $HOME/$file ] && [ $FORCE -eq 1 ]; then # file exists and force set, overwrite
+  elif [ -w $HOME/$file ] && [ $FORCE -eq 1 ]; then # file exists and force set, overwrite
     echo "overwriting ~/$file"
     ln -fs $DIR/$file $HOME
-  else # file exists, skip
+  elif [ -w $HOME/$file ] && [ $FORCE -ne 1 ]; then # file exists and force not set, skip
     echo "~/$file exists, skipping..."
+  else # file exists and is not writeable or other issue eg it's a directory
+    echo "couldn't write ~/$file, skipping"
   fi
 
 done < $filename
